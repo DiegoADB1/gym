615481d3dce66253d1c00e0161293e5cd8e3509c
Diego <diegoaragao465@gmail.com>
2023-06-17 03:01:59 -0300

    Add format date in preview

--- src/treino/form/TreinoPreview.js
+++ src/treino/form/TreinoPreview.js
@@ -1,4 +1,9 @@
 function TreinoPreview({ formInputData }) {
+    const formatDate = (dateString) => {
+        const options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric"}
+        return new Date(dateString).toLocaleDateString(undefined, options)
+      }
+
     return (
       <div className="container">
         <div className="row border p-2 shadow-lg p-3 mb-5 bg-white rounded">
@@ -12,7 +17,7 @@ function TreinoPreview({ formInputData }) {
           </p>
           <p className="border mt-1">
             <span className="fw-bold">Dia e hora:</span>&nbsp;
-            <span className="text-muted">{formInputData.time} </span>
+            <span className="text-muted">{formInputData.time == "" ? "" : formatDate(formInputData.time)} </span>
           </p>
         </div>
       </div>
